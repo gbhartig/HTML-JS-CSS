64840d9933b70c828438f1080e7588d390be5269
anshika <abanshika3991@gmail.com>
2025-11-22 19:08:53 +0530

    Assignement_1- student-info changes has been done!

---
+++ Assignment_1/StudentInfo.js
@@ -0,0 +1,8 @@
+let name = prompt("Enter your name:");
+let age = prompt("Enter your age:");
+let course = prompt("Enter your course:");
+
+console.log("Student Info:");
+console.log("Name: " + name, "Age: " + age, "Course: " + course);
+
+alert("Student Info:\nName: " + name + "\nAge: " + age + "\nCourse: " + course);
